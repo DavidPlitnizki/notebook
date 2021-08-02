6d4713fdf91bbaf8f735457808def7260d36206d
David <dplitnizki@gmail.com>
2021-08-02 23:18:54 +0300

    Updates

---
+++ precache-manifest.70b065ffd9c608ac389ea45d1981deb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d391b7c9f9968bd412bd2d6174ad10",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "f568d87b7a4ececc61f3",
+    "url": "/notebook/static/css/main.535f354c.chunk.css"
+  },
+  {
+    "revision": "98f0a7f0a3039dd5d6b3",
+    "url": "/notebook/static/js/2.ac2f469a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/notebook/static/js/2.ac2f469a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f568d87b7a4ececc61f3",
+    "url": "/notebook/static/js/main.7a99b29d.chunk.js"
+  },
+  {
+    "revision": "a76ee83b5c2ec9cd422f",
+    "url": "/notebook/static/js/runtime-main.f0825a35.js"
+  }
+]);
